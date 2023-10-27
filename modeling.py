675f50ce1eef2befa4b5a1b27ae98045e63de935
whoisjones <jonas.golde@gmail.com>
2023-10-27 11:28:27 +0200

    flair draft

---
+++ modeling.py
@@ -0,0 +1,12 @@
+import torch
+
+
+class SetFitDecoder(torch.nn.Module):
+
+    def __init__(
+        self,
+    ):
+        raise NotImplementedError #TODO: Implement SetFit
+
+    def forward(self, inputs: torch.Tensors):
+        raise NotImplementedError #TODO: Implement SetFit
